f6ce05c37773c0163143323454995755b7e1a44c
liwei-liang <liwei.liang333@gmail.com>
2018-06-05 00:55:41 +0200

    Update NumberComplement.java

--- Number Complement476/NumberComplement.java
+++ Number Complement476/NumberComplement.java
@@ -25,4 +25,20 @@ public class NumberComplement {
 		System.out.println((int) Math.pow(2,31));
 
 	}
+	//for example:
+//100110, its complement is 011001, the sum is 111111. So we only need get the min number large or equal to num, then do substraction
+
+    public int findComplement(int num) 
+    {
+        int i = 0;
+        int j = 0;
+        
+        while (i < num)
+        {
+            i += Math.pow(2, j);
+            j++;
+        }
+        
+        return i - num;
+    }
 }
